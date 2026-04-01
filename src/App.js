47e44714e16a04f812de494b27f6d6735cfe7c74
HANE48 <hun2me2ne@gmail.com>
2026-04-01 17:36:02 +0900

    main 프로그램

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
