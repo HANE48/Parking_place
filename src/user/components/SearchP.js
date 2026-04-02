2e4c7dd1a425150c9460f4307bb504b4aa7865db
H-JEE1216 <rosemary4132@naver.com>
2026-04-02 23:10:11 +0900

    메인화면 보완

--- src/user/components/SearchP.js
+++ src/user/components/SearchP.js
@@ -4,11 +4,13 @@ import { Link, Route, Routes } from "react-router-dom";
 import PriceList from "./PriceList";
 
 const SearchP = ()=>{
-    
+    //사용자에게 입력받은 주소를 저장할 addr변수
     const [addr, setAddr] = useState('')
 
     //원하는 지역의 주차장 리스트 저장하는 list변수
     const [list, setList] = useState([])
+
+    const [buttons, setButtons] = useState(false)
     
     //list변수에 검색한 결과를 배열로 저장
     const checkList = ()=>{
@@ -18,10 +20,19 @@ const SearchP = ()=>{
         const filtered = Parking_info.DATA.filter( (item)=> item.addr.includes(addr) )
 
         setList(filtered)
+        setButtons(true)
 
     }
 
-    
+    //검색하기 전 메인화면에 출력할 화면
+    const forEmptyList = ()=>{
+        return(
+            <div>
+                <h3>원하는 지역을 검색하고</h3>
+                <h3>그 지역의 주차장 현황을 확인해보세요</h3>
+            </div>
+        )
+    }
 
     //검색 결과 출력하는 메서드
     const showList = list.map( (res)=>{
@@ -50,16 +61,16 @@ const SearchP = ()=>{
                 <input value={addr}
                         onChange={(e)=>{setAddr(e.target.value)}}
                         placeholder="지역명을 입력하세요(구/도로명)"/>
+            
+                <Link to={"/search"}><button onClick={checkList}>찾기</button></Link>
             </div>
-            <div>
-                <button onClick={checkList}>찾기</button>
-            </div>
-
+            { buttons ?
             <div>
                 <button> <Link to="/price">가격순</Link></button>
                 <button> <Link to="/space">주차공간순</Link></button>
                 <button> <Link to="/size">주차장규모순</Link></button>
-            </div>
+            </div> : <div></div>
+}
 
             {/* <div>
                 {showList}
@@ -70,7 +81,9 @@ const SearchP = ()=>{
                     <Route path="/price" element={<PriceList list={list}/>} />
                     <Route path="/space" element={<div>주차공간순</div>} />
                     <Route path="/size" element={<div>주차장규모순</div>} />
-                    <Route path="/" element={showList} />
+                    <Route path="/search" element={showList} />
+                    <Route path="/" element={forEmptyList()} />
+                    
                 </Routes>
                 
             </div>
