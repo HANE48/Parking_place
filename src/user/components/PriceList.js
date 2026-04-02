c989dadb2bb91170fd494a809dff253aab28a951
rahellyberry <rahel0212@gmail.com>
2026-04-02 16:31:27 +0900

    Modify PriceList to accept and sort a list prop
    Refactor PriceList component to accept a list prop and sort it by price.

--- src/user/components/PriceList.js
+++ src/user/components/PriceList.js
@@ -1,10 +1,31 @@
 import React from "react";
 
-const PriceList = ()=>{
+const PriceList = ({list})=>{
+    
+    const sortedList = [...list].sort( (a, b) => {
+       
+       const priceA = Number(a.bsc_prk_crg)  
+        console.log('1')
+       const priceB = Number(b.bsc_prk_crg)
+       console.log('2')
 
+       return priceA - priceB })
+
+       console.log(sortedList)
+
+       
     return(
         <div>
-
+            <h2>주차 요금 목록</h2>
+            <ul> 
+                {              
+                sortedList.map((item, index) => (
+                    <li key={index}>
+                      {item.bsc_prk_crg}
+                    </li>                    
+                ) ) 
+                }
+            </ul>
         </div>
     )
 }
