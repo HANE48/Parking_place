9c368dbd6fbafb91c9abb6166e6351c406edc3e3
seungyeon8802 <seungyeon8302@gmail.com>
2026-04-02 17:41:19 +0900

    manager 관리페이지 렌더링

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import ManagerMain from './manager/manager_main';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-
+        <ManagerMain />
       </div>
     </BrowserRouter>
   );
---
+++ src/manager/manager_main.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './manager_main.css'; // CSS 파일 연결
+import rawData from './ParkingData.json';
+
+function ManagerMain() {
+
+
+
+  return(
+    <div>
+
+
+
+
+    </div>
+  )
+}
+export default ManagerMain;
