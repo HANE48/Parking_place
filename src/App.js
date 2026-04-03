9dbb242e383e6107a54bd8c1ee9a2fc130b0fdbb
911carpediem <carpediem9011@gmail.com>
2026-04-03 13:55:06 +0900

    park

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import ManagerMain from './manager/manager_main';
+import ParkingTable from './manager/ParkingTable';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <ManagerMain />
+        {/* <ManagerMain /> */}
+        <ParkingTable/>
       </div>
     </BrowserRouter>
   );
--- src/manager/ParkingTable.js
+++ src/manager/ParkingTable.js
@@ -3,12 +3,14 @@ import ParkingData from './ParkingData';
 
 const ParkingTable = () => {
     const id = 171730;
-    const [data, setData] = useState('');
-    // ParkingData.Data로 접근해서 json파일의 내용을 사용
+    const [data, setData] = useState(ParkingData.DATA.filter
+        (parking => parking.pklt_cd === id)
+    );
+    // ParkingData.DATA로 접근해서 json파일의 내용을 사용
     //[+]버튼 누르면 실행되는 함수
     const handleIncrease = (index) => {
         const newData = [...data]; // 원본데이터 복사
-        if(newData[index].now_prk_vhcl_cnt <  newData[index].tpkct){
+        if (newData[index].now_prk_vhcl_cnt < newData[index].tpkct) {
             newData[index].now_prk_vhcl_cnt += 1; // 현재 주차수 +1
             setData(newData); //현재 상태(State) 업데이트
         }
@@ -17,18 +19,17 @@ const ParkingTable = () => {
     //[-]버튼 누르면 실행되는 함수
     const handleDecrease = (index) => {
         const newData = [...data];
-        if(newData[index].now_prk_vhcl_cnt > 0){
+        if (newData[index].now_prk_vhcl_cnt > 0) {
             newData[index].now_prk_vhcl_cnt -= 1; //현재 주차수 -1
             setData(newData);
         }
     };
 
-    return(
+    return (
         <table>
             <thead>
                 <tr>
                     <th>주차장명</th>
-                    <th>현황 </th>
                     <th>현황 (현재/전체)</th>
                     <th>입/출차 관리</th>
                     <th>운영시간(평일)</th>
@@ -37,25 +38,22 @@ const ParkingTable = () => {
             </thead>
 
             <tbody>
-                {data.map((Parking,index)=>(
+                {data.map((parking, index) => (
                     <tr>
-                        <td>{Parking.pklt_nm}</td>
-                        <td>{Parking.now_prk_vhcl_cnt} / {Parking.tpkct}</td>
+                        <td>{parking.pklt_nm}</td>
+                        <td>{parking.now_prk_vhcl_cnt} / {parking.tpkct}</td>
                         <td>
                             {/* +, - 버튼 활용 */}
                             <button onClick={() => handleIncrease(index)}>+</button>
                             <button onClick={() => handleDecrease(index)}>-</button>
                         </td>
-                        <td>{Parking.wd_oper_bgng_tm} ~ {Parking.wd_oper_end_tm}</td>
-                        <td>{Parking.bsc_prk_crg}원</td>
+                        <td>{parking.wd_oper_bgng_tm} ~ {parking.wd_oper_end_tm}</td>
+                        <td>{parking.bsc_prk_crg}원</td>
                     </tr>
                 ))}
             </tbody>
         </table>
     )
 }
-
-// const available = tpkct - now_prk_vh //주차가능수
-// const occupancyRate = (now_prk_vhcl_cnt / tpkct) * 100; //혼잡도
 
 export default ParkingTable;
