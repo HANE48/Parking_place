64cbd7ae8459ae89db2c4644fdaaba98dacc4203
H-JEE1216 <rosemary4132@naver.com>
2026-04-03 17:42:20 +0900

    주차장 상세보기창 영업시간 출력 수정

--- src/user/components/Detail.js
+++ src/user/components/Detail.js
@@ -12,14 +12,24 @@ const Detail = ({list})=>{
     } )
 
     const showList = choosedItem.map( (res) =>{
+
+        let DayOpenHr = res.wd_oper_bgng_tm.slice(0, 2);
+        let DayOpenMin = res.wd_oper_bgng_tm.slice(2, 4);
+        let DayEndHr = res.we_oper_end_tm.slice(0, 2);
+        let DayEndMin = res.we_oper_end_tm.slice(2, 4);
+        let WeekOpenHr = res.we_oper_bgng_tm.slice(0, 2);
+        let WeekOpenMin = res.we_oper_bgng_tm.slice(2,4);
+        let WeekEndHr = res.we_oper_end_tm.slice(0, 2);
+        let WeekEndMin = res.we_oper_end_tm.slice(2, 4);
+        
         return(
             <div>
                 <div>
                     <h2>상세정보</h2>
                     {res.pklt_nm}<br/>
                     주소 : {res.addr}<br/>
-                    평일 운영시간 : {res.wd_oper_bgng_tm} ~ {res.wd_oper_end_tm} <br/>
-                    주말 운영시간 : {res.we_oper_bgng_tm} ~ {res.we_oper_end_tm} <br/>
+                    평일 운영시간 : {DayOpenHr}:{DayOpenMin} ~ {DayEndHr}:{DayEndMin} <br/>
+                    주말 운영시간 : {WeekOpenHr}:{WeekOpenMin} ~ {WeekEndHr}:{WeekEndMin} <br/>
                     월 정기권 금액 :  
                     { res.prd_amt == 0 ? "월 정기권은 주차장으로 문의 해주세요"
                         : <div>{res.prd_amt}원</div>
