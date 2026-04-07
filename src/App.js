dc338303d1fd2f4a0f27f05357546289181225fb
HANE48 <hun2me2ne@gmail.com>
2026-04-07 17:44:25 +0900

    App.js 수정2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import Parking_info from './Parking_info.json';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import SearchP from './user/components/SearchP.js';
-
 
 function App() {
   return (
