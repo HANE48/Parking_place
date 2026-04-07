9bf5eaeec3698e9ecc9ff3896a9a3b034fa04b57
H-JEE1216 <rosemary4132@naver.com>
2026-04-07 11:21:34 +0900

    메인(searchP)에서 지역별 목록 컴포넌트 분리

---
+++ src/user/components/AddressList.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const AddressList = ({list})=>{
+
+    return(
+        <div>
+            <h2>지역별 목록</h2>
+            <ul>
+                {
+                    list.map( (res)=>{
+                        let availableLots = Number(res.tpkct)-Number(res.now_prk_vhcl_cnt);
+
+                        //availableLots(주차가능수)가 0보다 작으면 0으로 표시
+                        if( availableLots <= 0 ){
+                            availableLots = 0
+                        }
+
+                        return(
+                            <Link to={'/detail/' + res.pklt_cd}>
+                                <li>
+                                    {res.pklt_nm}<br/>
+                                    {res.addr}<br/>
+                                    주차가능 수 : {availableLots}
+                                </li>
+                            </Link>
+                        )
+                    } )
+                }
+            </ul>
+        </div>
+    )
+
+}
+
+export default AddressList
