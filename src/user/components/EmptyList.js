f2f196f016bdea63b1db6c89cebc739fab144395
H-JEE1216 <rosemary4132@naver.com>
2026-04-07 16:51:13 +0900

    메인 홈화면 이미지 추가

--- src/user/components/EmptyList.js
+++ src/user/components/EmptyList.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Parking2 from "../img/Parking2.png";
 
 const EmptyList = ()=>{
 
     return(
         <div>
-            <h3>원하는 지역을 검색하고</h3>
-            <h3>그 지역의 주차장 현황을 확인해보세요</h3>
+            <h3 className="first_str">원하는 지역을 검색하고</h3>
+            <h3 className="first_str">그 지역의 주차장 현황을 확인해보세요</h3>
+            <img src={Parking2}/>
         </div>
     )
 
