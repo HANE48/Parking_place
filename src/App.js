0c32984f1fe48c8e24e5098050efe08881abbd14
HANE48 <hun2me2ne@gmail.com>
2026-04-07 16:39:01 +0900

    합칠 준비를 위한 app.js 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-import User_main from './user/User_main.js';
-import Manager_main from './manager/Manager_main.js';
-import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Parking_info from './Parking_info.json';
 
@@ -16,7 +13,7 @@ function App() {
         <br/>
         <Link to='/manager'>관리자창</Link>
         <Routes>
-          <Route path='/' element={<ParkingApp />} />
+          <Route path='/' element={<div><p>검색</p></div>} />
           <Route path='/manager' element={<Login />} />
         </Routes>
       </div>
@@ -33,25 +30,5 @@ function Login(){
 
 }
 
-function ParkingApp() {
-  const arr = Parking_info.DATA.map((item, index)=>{
-    return(
-      <li key={index} style={{border:'1px solid black' }}>
-        주차장명: {item.pklt_nm}<br/>
-        주차면 수: {item.tpkct}<br/>
-        현재 주차 차량수: {item.now_prk_vhcl_cnt}
-      </li>
-    );
-  })
-
-  return (
-    <div style={{ padding: '20px' }}>
-      <input type='text' placeholder='주차장 검색 '/>
-      <ul>
-        {arr}
-      </ul>
-    </div>
-    );
-}
 
 export default App;
