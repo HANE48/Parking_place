efdd9656c4ed53eddaf166422f3fa8f97773da81
HANE48 <hun2me2ne@gmail.com>
2026-04-07 17:28:12 +0900

    merge를 위한 app.js 수정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-import SearchP from './user/components/SearchP.js';
+import { useState, useEffect } from 'react';
+import Parking_info from './Parking_info.json';
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <SearchP/>
+        <Link to='/'>유저창</Link>
+        <br/>
+        <Link to='/manager'>관리자창</Link>
+        <Routes>
+          <Route path='/' element={<div><p>검색</p></div>} />
+          <Route path='/manager' element={<Login />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
 }
+
+function Login(){
+  return(
+    <div>
+      <p>로그인 화면</p>
+    </div>
+  );
+
+}
+
 
 export default App;
