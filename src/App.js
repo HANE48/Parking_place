82a7731444fbd4380adb2a98a520be80392a7eae
HANE48 <hun2me2ne@gmail.com>
2026-04-08 09:15:32 +0900

    css 개선

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Link} from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import SearchP from './user/components/SearchP.js';
 import AdminLogin from './manager/AdminLogin.js';
 import ManagerMain from './manager/manager_main.js';
+import './App.css';
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Link to='/'>홈</Link>
-      &nbsp;&nbsp;&nbsp;&nbsp;
-      <Link to='/manager/login'>관리자 로그인</Link>
+      <header>
+        <Link to='/'>홈</Link>
+        <Link to='/manager/login'>관리자 로그인</Link>
+      </header>
       <Routes>
         <Route path="/*" element={<SearchP />} />
         <Route path="/manager/login/*" element={<AdminLogin />} />
