54d9055932ea2aefc39792a3543c071e781f27d8
H-JEE1216 <rosemary4132@naver.com>
2026-04-08 11:20:01 +0900

    메인css추가수정

--- src/user/components/SearchP.js
+++ src/user/components/SearchP.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Parking_info from "../../Parking_info.json";
-import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import { href, Link, Route, Routes, useNavigate } from "react-router-dom";
 import PriceList from "./PriceList";
 import LotsList from "./LotsList";
 import Detail from "./Detail";
@@ -23,7 +23,7 @@ const SearchP = ()=>{
     const [addr, setAddr] = useState('')
 
     //원하는 지역의 주차장 리스트 저장하는 list변수
-    const [list, setList] = useState([])s
+    const [list, setList] = useState([])
 
     const [buttons, setButtons] = useState(false)
 
@@ -50,16 +50,26 @@ const SearchP = ()=>{
         setList(filtered)
         setButtons(true)
         navigate('/search');
+        setAddr('')
 
+    }
+
+    const toMain = ()=>{
+        setList([])
+        setIsSearched(false)
+        navigate('/')
     }
 
     return(
 
         <div className="main-box">
             <div>
-                <img src={final_sp} 
-                style={{width : '938.98px',
-                        height : '862.99'}}/>              
+                <a onClick={()=>{toMain()}} style={{cursor : 'pointer'}}
+                            alt="Seoul Parking Banner">
+                    <img src={final_sp} 
+                    style={{width : '938.98px',
+                        height : '862.99'}}/>
+                </a>              
             </div>
 
             <div className="inputSection">
@@ -71,6 +81,7 @@ const SearchP = ()=>{
                 
                     <button onClick={checkList} className="find">
                         <img src={search_pic_removebg_preview}
+                            alt="Main Parking Image"
                             style={{width : '30px',
                                     height : '30px', 
                                     mixBlendMode: 'multiply'}}/>
@@ -79,17 +90,17 @@ const SearchP = ()=>{
             </div>
 
             {/* 사용자가 이미 지역을 검색했는가? y : n */}
-            { buttons ?
-            <div className="btn">
-                <button> <Link to="/search">지역별</Link></button>
-                <button> <Link to="/price">가격순</Link></button>
-                <button> <Link to="/space">주차공간순</Link></button>
-                <button> <Link to="/size">주차장규모순</Link></button>
-            </div> : null
-            }
+            {buttons && (
+                <div className="btn">
+                    <button><Link to="/search">지역별</Link></button>
+                    <button><Link to="/price">가격순</Link></button>
+                    <button><Link to="/space">주차공간순</Link></button>
+                    <button><Link to="/size">주차장규모순</Link></button>
+                </div>
+            )}
 
 
-            <div>
+            <div className="content-area" style={{ width: '100%', maxWidth: '1000px', marginTop: '40px' }}>
                 <Routes>
                     <Route path="/price" element={<PriceList list={list}/>} />
                     <Route path="/space" element={<LotsList list={list}/>} />
@@ -98,7 +109,7 @@ const SearchP = ()=>{
                     <Route path="/detail/:id" element={<Detail list={list}/>}/>
                     {/* 유저가 한 번이라도 "찾기" 버튼을 눌렀다면 EmptyList를 출력하지 않게 함. */}
                     <Route path="/" element={ isSearched? 
-                            <div className="errorMsg">{searchError} <img src={Parking2}/></div> : <EmptyList/>}/>
+                            <div className="errorMsg">{searchError} <img className="mainImg" src={Parking2}/></div> : <EmptyList/>}/>
                 </Routes>
                 
             </div>
