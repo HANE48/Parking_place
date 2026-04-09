3b23b60dcd28b8fc41f072a4c29b007df0d2c93e
HANE48 <hun2me2ne@gmail.com>
2026-04-09 16:13:41 +0900

    검색 실패시 보이던 이미지를 밑으로 변경!

--- src/user/components/NoResult.js
+++ src/user/components/NoResult.js
@@ -1,19 +1,11 @@
 import React from 'react';
+import Parking2 from '../img/Parking2.png'
 
 const NoResult = ({ message }) => {
     return (
         <div className="no-result-container">
             <div className="no-result-content">
-                {/* 1. 시각적 일러스트 */}
-                <div className="illustration-wrapper">
-                    {/* 
-                      * <img src={Parking2} alt="No Results" className="no-result-img" /> 
-                      * 이미지가 검색결과가 없습니다 부분을 가려서 지웠어요!
-                      */}
-                    <div className="ghost-shadow"></div>
-                </div>
-
-                {/* 2. 에러 메시지 */}
+                {/* 1. 에러 메시지 */}
                 <h3 className="no-result-text">{message}</h3>
                 <p className="no-result-subtext" style={{color:'black'}}>
                     {/* 스타일이 이상하게 먹어서(글씨가 흰색이라 안보암) 글자색깔 검은색으로 직접 줬어요! */}
@@ -21,7 +13,7 @@ const NoResult = ({ message }) => {
                     구 단위(예: 강남구) 또는 도로명으로 다시 검색해 보세요.
                 </p>
 
-                {/* 3. 사용자 도움말 (칩 디자인) */}
+                {/* 2. 사용자 도움말 (칩 디자인) */}
                 <div className="search-tips">
                     <span>추천 검색어:</span>
                     <div className="tip-chips">
@@ -31,6 +23,13 @@ const NoResult = ({ message }) => {
                         <span className="chip">#잠실역</span>
                     </div>
                 </div>
+                <br />
+                {/* 3. 시각적 일러스트 */}
+                <div className="illustration-wrapper">
+                    <img src={Parking2} alt="No Results" className="no-result-img" /> 
+                    <div className="ghost-shadow"></div>
+                </div>
+
             </div>
         </div>
     );
